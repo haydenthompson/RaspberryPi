ad8501271d5477e7b786cc3e765ef6d8f01cf02b
haydenthompson <85725069+haydenthompson@users.noreply.github.com>
2021-06-10 22:24:29 -0700

    Create welcometest.py

---
+++ welcometest.py
@@ -0,0 +1,19 @@
+from sense_hat import SenseHat
+from time import sleep
+
+print("Hello! Welcome to the Raspimon traning academy. Are you ready to code?")
+
+#how to comment on python
+sleep (2)
+
+print ("Let's get started.")
+
+sleep (1)
+
+coder_name = input("What is your name?")
+
+sleep (1)
+
+print (f"Hello, {coder_name}. Thank you for joining. Prepare to learn a lot!")
+
+
